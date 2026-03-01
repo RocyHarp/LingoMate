2f3dd132bfe0e06d4e85719e2d57aeea72996f4c
RocyHarp <deniskovaluk0992@gmail.com>
2026-03-02 01:01:52 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,12 +2,12 @@ import database as db
 
 def get_xp_progress(user_id):
     stats = db.get_statistics(user_id)
-    words_count = len(db.get_all_words(user_id))
     
-    # Витягуємо бонуси
+    # 🔥 Більше ніяких len(get_all_words)! Беремо тільки одну цифру:
+    words_count = db.get_word_count(user_id)
+    
     bonus_xp = db.get_user_bonus_xp(user_id)
     
-    # Базові XP + Бонусні XP
     total_xp = (words_count * 10) + (stats['translations'] * 2) + (stats['correct'] * 5) + bonus_xp
     
     level = (total_xp // 1000) + 1
@@ -15,3 +15,8 @@ def get_xp_progress(user_id):
     progress_width = (current_xp / 1000) * 100
     
     return level, current_xp, progress_width
+
+# 🔥 Швидка формула для друзів (щоб не робити зайвих запитів до бази)
+def calc_level_from_raw_data(words_count, translations, correct, bonus_xp):
+    total_xp = (words_count * 10) + (translations * 2) + (correct * 5) + bonus_xp
+    return (total_xp // 1000) + 1
